ef715f31c343e729db606f2e0042de6f9eb752f3
github-trial <stefysarah.v@gmail.com>
2022-09-07 16:47:35 +0200

    end section1-5 basics python

---
+++ py-test/section6/function.py
@@ -0,0 +1,15 @@
+import re
+from unittest import result
+
+
+def multiply(x, y):
+    result = x * y
+    return result
+
+
+answer = multiply(10.5, 4)
+print(answer)
+
+for val in range(1, 5):
+    two_times = multiply(2, val)
+    print(two_times)
